367812b02425b76c805ffeb9e75e4327563f1f5d
davechai <davecqx@gmail.com>
2020-08-27 00:23:50 +0800

    mikro-orm setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello")
+import { MikroORM } from "@mikro-orm/core"
+
+import { __prod__ } from './constants'
+import { Post } from "./entities/Post"
+import mikroConfig from './mikro-orm.config'
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig)
+  await orm.getMigrator().up()
+  // const post = orm.em.create(Post, {title: "first post"})
+  // await orm.em.persistAndFlush(post)
+
+  // const posts = await orm.em.find(Post, {id: 3})
+  // console.log(posts)
+}
+
+main().catch(err => {
+  console.error(err)
+})
