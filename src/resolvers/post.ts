d22e50d6c995ad243781a200cba7bdf26cb6431a
davechai <davecqx@gmail.com>
2020-09-24 22:39:07 +0800

    post-pagination

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -2,6 +2,7 @@ import { Resolver, Query, Arg, Int, Mutation, UseMiddleware, InputType, Field, C
 import { MyContext } from "../types";
 import { isAuth } from "../utils/isAuth";
 import { Post } from "../entities/Post";
+import { getConnection } from "typeorm";
 
 @InputType()
 class PostInput {
@@ -15,8 +16,25 @@ class PostInput {
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(): Promise<Post[]> {
-    return Post.find();
+  async posts(
+    @Arg("limit", () => Int) limit: number,
+    @Arg("cursor", () => String, { nullable: true}) cursor: string | null
+  ): Promise<Post[]> {
+    const realLimit = Math.min(50, limit)
+
+    const qb = getConnection()
+    .getRepository(Post)
+    .createQueryBuilder('p')
+    .orderBy('"createdAt"', "DESC")
+    .take(realLimit)
+
+    if(cursor) {
+      qb.where('"createdAt" < :cursor', {
+        cursor: new Date(parseInt(cursor))
+      })
+    }
+
+    return qb.getMany();
   }
 
   @Query(() => Post, { nullable: true })
